a003de47584b1c4c5db42c74322188d854b5552d
A-nubhav <m.mratunjay123@gmail.com>
2025-03-26 18:33:11 +0530

    second

--- nodemailer.js
+++ nodemailer.js
@@ -1,7 +1,5 @@
-// const express = require("express");
 const nodemailer = require("nodemailer");
-// const twilio=require("twilio");
-// const otpgenerator=require("otp-generator");
+
 const { token } = require("./autherization");
 
 const {OtpSchema}=require("./dbconnection");
@@ -74,23 +72,6 @@ const verifyOtp=async(req,res)=>{
         res.status(400).json({msg:error});
     }
 }
-// using twilio service by twilio number 
-// const forgotPassword1=async(req,res)=>{
-//     const accountSid="AC02453baf2e82ca8053c1cd1a0a8fcc80";
-//     const authToken="03cebf9f7e02bd0daae3f9295826d1d5";
-//     const client=new twilio(accountSid,authToken);
-
-//     client.messages.create({
-//         body:"otp verification code 12344",
-//         from:"+1 251 351 7659",
-//         to:"+918090064743"
-//         // to:"+917268034706"
-//     }).then((messages)=>{
-//         console.log("message sent " + messages.sid);
-//     }).catch((error)=>{
-//         console.error(error);
-//     });
-// }
 
 module.exports={
     forgotPassword,
